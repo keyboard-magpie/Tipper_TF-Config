0caa8973887ac0118b6d52de04e42ee9cb15d9eb
MarvFPV <36986917+MarvFPV@users.noreply.github.com>
2022-01-07 17:59:49 +0100

    test3

--- boards/arm/tipper_tf/tipper_tf_status_screen.c
+++ boards/arm/tipper_tf/tipper_tf_status_screen.c
@@ -90,10 +90,10 @@ lv_obj_t *zmk_display_status_screen() {
 /*                  0); */
 /* #endif */
     
-    lv_obj_t * tipperlogo_icon;
-    tipperlogo_icon = lv_img_create(screen, NULL);
-    lv_img_set_src(tipperlogo_icon, &tipperlogo);
-    lv_obj_align(tipperlogo_icon, NULL, LV_ALIGN_IN_TOP_MID, 0,0);
+    //lv_obj_t * tipperlogo_icon;
+    //tipperlogo_icon = lv_img_create(screen, NULL);
+    //lv_img_set_src(tipperlogo_icon, &tipperlogo);
+    //lv_obj_align(tipperlogo_icon, NULL, LV_ALIGN_IN_TOP_MID, 0,0);
     
     return screen;
 }
